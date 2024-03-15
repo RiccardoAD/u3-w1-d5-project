70754522756d61d8cf6f71aa7e42d622192085d6
RiccardoAD <rick_95@libero.it>
2024-03-15 17:24:22 +0100

    style

--- src/components/FilmBody.jsx
+++ src/components/FilmBody.jsx
@@ -7,53 +7,40 @@ import Dropdown from "react-bootstrap/Dropdown";
 
 const MainContainer = function () {
   return (
-    <div className="container-fluid px-4 bg-dark text-white">
-      <Container className="d-flex justify-content-between">
-        <div className="d-flex">
-          <h2 className="mb-4">TV Shows</h2>
+    <Container fluid className="px-4 bg-dark text-white">
+      <Container  fluid className="d-flex justify-content-between">
+        <Container className="d-flex me-3">
+          <Row className="d-flex justify-content-start "><h2 className="mb-4">TV Shows</h2></Row>
           <div className="btn-group" role="group">
-            <div className="dropdown ms-4 mt-1">
-              <button
-                type="button"
-                className="btn btn-secondary btn-sm dropdown-toggle rounded-0"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-                style={{ backgroundColor: "#221f1f" }}
+          <div>
+            <Dropdown className="ms-3">
+              <Dropdown.Toggle
+                id="dropdown-basic"
+                className="bg-dark border border-1  border-secondary mt-1   rounded-0 p-1"
               >
-                Genres
-              </button>
-              <ul className="dropdown-menu">
-                <li>
-                  <a className="dropdown-item" href="#link">
-                    Comedy
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#link">
-                    Drama
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#link">
-                    Thriller
-                  </a>
-                </li>
-              </ul>
-            </div>
+                Geners
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">Comedy</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Drama</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Thriller</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
           </div>
-        </div>
-        <div>
+          </div>
+        </Container>
+        <Col className="d-flex justify-content-end">
           <i className="bi bi-grid icons pl-3"></i>
           <i className="bi bi-grid-3x3 icons"></i>
-        </div>
+        </Col>
       </Container>
       <Row className="text-start"><h4>Trending Now</h4></Row>
       <FilmRow seriesToSearch="Pirates of the Caribbean" />
       <Row className="text-start"><h4>Watch Again</h4></Row>
-      <FilmRow seriesToSearch="lord of the rings" />
+      <FilmRow seriesToSearch="Witch" />
       <Row className="text-start"><h4>New realise</h4></Row>
       <FilmRow seriesToSearch="scary" />
-    </div>
+    </Container>
   );
 };
 
